0a1f34675dae6d54a789abfdcde3a0333d62abe1
DATVenancio <danielvenancioif@gmail.com>
2024-05-29 09:03:47 +0200

    Frontend removed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO,send
 app = Flask(__name__)
 
 
-socketio = SocketIO(app,)
+socketio = SocketIO(app,cors_allowed_origins="*")
 
 #socket messages
 @socketio.on("message")
@@ -13,19 +13,6 @@ def handle_message(message):
 
 @socketio.on("user_connection")
 def user_connection(username):
-    return render_template("index.html",username=username)
-
-
-
-#routes
-@app.route("/")
-@app.route("/chat", methods=["GET"])
-def login():
-    return render_template("login.html")
-
-@app.route("/chat", methods=["POST"])
-def index():
-    username = request.form.get("username")
     return render_template("index.html",username=username)
 
 
